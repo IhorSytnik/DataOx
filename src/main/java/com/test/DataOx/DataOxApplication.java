51adb91658d930871873c5699df356af129c96d4
Kintys <kintys.sytnik@gmail.com>
2025-08-27 12:35:37 +0300

    Initial commit

---
+++ src/main/java/com/test/DataOx/DataOxApplication.java
@@ -0,0 +1,13 @@
+package com.test.DataOx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataOxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataOxApplication.class, args);
+	}
+
+}
